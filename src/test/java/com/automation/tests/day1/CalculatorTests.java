6d6afeff5a0b075104267774300a9ef0c2caf8e5
cybertek <github@cybertekschool.com>
2020-02-19 12:39:17 -0500

    calculator tests

--- src/test/java/com/automation/tests/day1/CalculatorTests.java
+++ src/test/java/com/automation/tests/day1/CalculatorTests.java
@@ -4,6 +4,7 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,6 +12,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
 public class CalculatorTests {
+    private AndroidDriver<MobileElement> driver;
 
     @Test
     public void test1() throws Exception {
@@ -18,11 +20,19 @@ public class CalculatorTests {
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
         desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "7.0");
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        //these 2 parameters are required if you want to automate existing application
         desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
         desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
-        AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+        driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+        MobileElement digit1 = driver.findElement(By.id("com.android.calculator2:id/digit_1"));
+        MobileElement digit2 = getDigit(2);
+
         Thread.sleep(3000);
         driver.closeApp();
 
+    }
+
+    public MobileElement getDigit(int digit) {
+        return driver.findElement(By.id("com.android.calculator2:id/digit_" + digit));
     }
 }
