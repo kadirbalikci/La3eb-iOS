46a11b43db92feab3cf32ab5f4ba6e70df437038
cybertek <github@cybertekschool.com>
2020-02-19 12:55:53 -0500

    calculator tests

--- src/test/java/com/automation/tests/day1/CalculatorTests.java
+++ src/test/java/com/automation/tests/day1/CalculatorTests.java
@@ -1,16 +1,19 @@
 package com.automation.tests.day1;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
-
+//10 minutes break
 public class CalculatorTests {
     private AndroidDriver<MobileElement> driver;
 
@@ -24,15 +27,35 @@ public class CalculatorTests {
         desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
         desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
         driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-        MobileElement digit1 = driver.findElement(By.id("com.android.calculator2:id/digit_1"));
-        MobileElement digit2 = getDigit(2);
+
+        WebElement digit1 = driver.findElement(By.id("com.android.calculator2:id/digit_1"));
+        WebElement digit2 = getDigit(2);
+        WebElement plus = driver.findElement(MobileBy.AccessibilityId("plus"));
+        WebElement equals = driver.findElementByAccessibilityId("equals");
+        WebElement result = driver.findElementById("com.android.calculator2:id/result");
+
+        digit1.click();//click on 1
+        digit2.click();//click on 2
+
+        plus.click();//click on +
+
+        digit2.click();//click on 2
+        digit2.click();//click on 2
+
+        equals.click();//click on equals
+
+        String actual = result.getText();//get text of result
+
+        Assert.assertEquals("34", actual);
+
+
 
         Thread.sleep(3000);
         driver.closeApp();
 
     }
 
-    public MobileElement getDigit(int digit) {
+    public WebElement getDigit(int digit) {
         return driver.findElement(By.id("com.android.calculator2:id/digit_" + digit));
     }
 }
