8e3fa58df4fcaee0fe9d6d23c02ddffeb1d3f522
cybertek <github@cybertekschool.com>
2020-02-20 12:20:41 -0500

    calculator tests

---
+++ src/test/java/com/automation/tests/day3/MobileWebTestAutomation.java
@@ -0,0 +1,37 @@
+package com.automation.tests.day3;
+
+import io.appium.java_client.remote.MobileBrowserType;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.*;
+
+import javax.print.DocFlavor;
+
+public class MobileWebTestAutomation {
+    //for mobile web automation, we don't use appium drivers
+    //we just use WebDriver or RemoteWebDriver
+    //RemoteWebDriver - it's a class, that implements WebDriver interface
+    //RemoteWebDriver - is a super class for all web drivers, like: ChromeDriver, FirefoxDriver, IEDriver, etc..
+    private RemoteWebDriver driver;
+
+    /**
+     * In this test, instead of desktop chrome browser, we gonna use
+     * mobile chrome browser.
+     * For testing, we will use vytrack
+     */
+    @Test
+    public void test1() throws Exception{
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", Platform.ANDROID);
+        desiredCapabilities.setCapability("platformVersion", "7.0");
+        desiredCapabilities.setCapability("deviceName", "Pixel_2");
+        desiredCapabilities.setCapability("automationName", "UiAutomator2");
+        desiredCapabilities.setCapability("browser", MobileBrowserType.CHROME);
+        driver = new RemoteWebDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+        driver.get("http://qa1.vytrack.com");
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
