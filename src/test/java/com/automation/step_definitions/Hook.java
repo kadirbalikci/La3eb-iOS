801d8a028410dea94c4f4cd3e9b618f7a2cfb8e9
cybertek <github@cybertekschool.com>
2020-02-20 16:38:05 -0500

    cucumber

---
+++ src/test/java/com/automation/step_definitions/Hook.java
@@ -0,0 +1,12 @@
+package com.automation.step_definitions;
+
+import com.automation.utilities.Driver;
+import io.cucumber.java.After;
+
+public class Hook {
+
+    @After
+    public void teardown() {
+        Driver.closeDriver();
+    }
+}
