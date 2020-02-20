bebf3e4156e02cd195dac14b1598891a15b06f72
cybertek <github@cybertekschool.com>
2020-02-20 16:32:39 -0500

    cucumber

---
+++ src/test/java/com/automation/pages/BasePage.java
@@ -0,0 +1,23 @@
+package com.automation.pages;
+
+import com.automation.utilities.Driver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    @AndroidFindBy(id = "com.etsy.android:id/btn_link")
+    private MobileElement getStarted;
+
+    public BasePage(){
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.get()), this);
+    }
+
+
+    public void clickOnGetStarted() {
+
+        getStarted.click();
+    }
+}
