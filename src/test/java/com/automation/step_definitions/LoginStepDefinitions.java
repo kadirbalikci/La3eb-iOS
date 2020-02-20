2b6d93bb15f0fd50b2a4fb9e9778064865426fb6
cybertek <github@cybertekschool.com>
2020-02-20 16:43:49 -0500

    cucumber

--- src/test/java/com/automation/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/automation/step_definitions/LoginStepDefinitions.java
@@ -1,4 +1,24 @@
 package com.automation.step_definitions;
 
+import com.automation.pages.LoginPage;
+import io.cucumber.java.en.*;
+
 public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
+
+    @Given("use clicks on get started")
+    public void use_clicks_on_get_started() {
+        loginPage.clickOnGetStarted();
+    }
+
+    @When("user logs in with etsy credentials")
+    public void user_logs_in_with_etsy_credentials() {
+
+    }
+
+    @Then("user verifies that etsy logo is displayed")
+    public void user_verifies_that_etsy_logo_is_displayed() {
+
+    }
+
 }
