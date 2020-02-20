02da508ed549e40b4f067bd50ca0e08023124d7b
cybertek <github@cybertekschool.com>
2020-02-20 16:24:43 -0500

    cucumber

--- src/test/java/com/automation/runners/CukesRunner.java
+++ src/test/java/com/automation/runners/CukesRunner.java
@@ -1,4 +1,17 @@
 package com.automation.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/automation/step_definitions",
+        plugin = {
+                "json:target/cucumber.json"
+        },
+        dryRun = true
+)
 public class CukesRunner {
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,3 +1,7 @@
 Feature: Login feature
   As user, I want to be able to log in with etsy account, as well as with google
   and facebook accounts.
+
+  Scenario: Login
+    Given user logs in with etsy credentials
+    Then user verifies that etsy logo is displayed
